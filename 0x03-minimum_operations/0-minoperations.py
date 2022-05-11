0d2d41e5d5bd60ac0a2d2345dac8ad3008139209
José Valdiviezo <luisj.valdiviezo20@gmail.com>
2022-05-11 12:18:12 -0500

    Update: for loop with <=

--- 0x03-minimum_operations/0-minoperations.py
+++ 0x03-minimum_operations/0-minoperations.py
@@ -4,17 +4,7 @@
 
 def minOperations(n):
     res = 0
-
-    if n == 7:
-        return 7
-    if n == 2:
-        return 2
-    if n == 3:
-        return 3
-    if n == 5:
-        return 5
-
-    for i in range(2, n):
+    for i in range(2, n + 1):
         while(n % i == 0):
             res += i
             n = n / i
