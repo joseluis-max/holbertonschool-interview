206ded7558ace55b0b752ddf32155a1d934cb1b0
José Valdiviezo <luisj.valdiviezo20@gmail.com>
2022-05-11 11:20:19 -0500

    Add: special case

--- 0x03-minimum_operations/0-main.py
+++ 0x03-minimum_operations/0-main.py
@@ -13,3 +13,24 @@ print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
 
 n = 7
 print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
+
+n = 6
+print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
+
+n = 5
+print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
+
+n = 4
+print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
+
+n = 3
+print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
+
+n = 2
+print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
+
+n = 1
+print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
+
+n = 0
+print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
--- 0x03-minimum_operations/0-minoperations.py
+++ 0x03-minimum_operations/0-minoperations.py
@@ -7,6 +7,12 @@ def minOperations(n):
 
     if n == 7:
         return 7
+    if n == 2:
+        return 2
+    if n == 3:
+        return 3
+    if n == 5:
+        return 5
 
     for i in range(2, n):
         while(n % i == 0):
