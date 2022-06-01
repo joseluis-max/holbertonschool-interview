8aee16ccdb48cd33a6312a617f4d2f6a9261d3d4
José Valdiviezo <luisj.valdiviezo20@gmail.com>
2022-06-01 12:41:04 -0500

    Add: log_parsing

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -5,24 +5,25 @@ import sys
 if __name__ == "__main__":
     counter = 1
     sum = 0
-    metrics = {}
+    metrics = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
     try:
         for line in sys.stdin:
             line = line.rstrip()
             line = line.split(" ")
             sum += int(line[-1])
-            try:
-                metrics[line[-2]] += 1
-            except KeyError:
-                metrics[line[-2]] = 1
+            code = line[-2]
+            if code in metrics:
+                metrics[code] += 1
             if counter % 10 == 0:
                 print("File size: {}".format(sum))
-                for key in sorted(metrics):
-                    print("{}: {}".format(key, metrics[key]))
+                for key, value in sorted(metrics.items()):
+                    if value:
+                        print("{}: {}".format(key, value))
             counter += 1
     except Exception:
         pass
     finally:
         print("File size: {}".format(sum))
-        for key in sorted(metrics):
-            print("{}: {}".format(key, metrics[key]))
+        for key, value in sorted(metrics.items()):
+            if value:
+                print("{}: {}".format(key, value))
