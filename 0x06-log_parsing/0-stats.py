5d3908d5db61eb8c6c58a587201badf237cb2ddd
José Valdiviezo <luisj.valdiviezo20@gmail.com>
2022-06-01 12:11:55 -0500

    Add: log_parsing

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -4,26 +4,26 @@
 
 import sys
 
-
-counter = 0
-sum = 0
-metrics = {}
-try:
-    for line in sys.stdin:
-        line = line.rstrip()
-        line = line.split(" ")
-        sum += int(line[-1])
-        try:
-            metrics[line[-2]] += 1
-        except KeyError:
-            metrics[line[-2]] = 1
-        counter += 1
-        if counter == 10:
-            counter = 0
-            print("File size: {:d}".format(sum))
-            for key in sorted(metrics):
-                print(f"{key}: {metrics[key]}")
-except KeyboardInterrupt:
-    print("File size: {:d}".format(sum))
-    for key in sorted(metrics):
-        print(f"{key}: {metrics[key]}")
+if __name__ == '__main__':
+    counter = 0
+    sum = 0
+    metrics = {}
+    try:
+        for line in sys.stdin:
+            line = line.rstrip()
+            line = line.split(" ")
+            sum += int(line[-1])
+            try:
+                metrics[line[-2]] += 1
+            except KeyError:
+                metrics[line[-2]] = 1
+            counter += 1
+            if counter == 10:
+                counter = 0
+                print("File size: {:d}".format(sum))
+                for key in sorted(metrics):
+                    print(f"{key}: {metrics[key]}")
+    except KeyboardInterrupt:
+        print("File size: {:d}".format(sum))
+        for key in sorted(metrics):
+            print(f"{key}: {metrics[key]}")
