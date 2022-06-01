ccfcfe15ee0fb8b5433d3e93f095c0c54339b43b
José Valdiviezo <luisj.valdiviezo20@gmail.com>
2022-06-01 09:19:47 -0500

    Add: log_parsing

---
+++ 0x06-log_parsing/0-stats.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+""" Write a script that reads stdin line by line and computes metrics:
+"""
+
+import sys
+
+
+counter = 0
+sum = 0
+metrics = {}
+try:
+    for line in sys.stdin:
+        line = line.rstrip()
+        line = line.split(" ")
+        sum += int(line[-1])
+        try: 
+            metrics[line[-2]] += 1
+        except KeyError:
+            metrics[line[-2]] = 1
+        counter += 1
+        if counter == 10:
+            counter = 0
+            print("File size: {:d}".format(sum))
+            for key in sorted(metrics):
+                print(f"{key}: {metrics[key]}")
+except KeyboardInterrupt:
+    print("File size: {:d}".format(sum))
+    for key in sorted(metrics):
+        print(f"{key}: {metrics[key]}")
