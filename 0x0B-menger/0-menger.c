fb29dd299fe4551a70e836d000ac7b145e0f30e4
José Valdiviezo <luisj.valdiviezo20@gmail.com>
2022-07-05 20:57:38 -0500

    Add: menger sponge

---
+++ 0x0B-menger/0-menger.c
@@ -0,0 +1,37 @@
+#include "menger.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+/**
+ * menger - draw 2D Menger Sponge
+ * @level: Menger sponge level
+ */
+void menger(int level)
+{
+	int i, j, size, row, col;
+	char sim;
+
+	size = pow(3, level);
+
+	for (i = 0; i < level; i++)
+	{
+		for (j = 0; j < level; j++)
+		{
+			row = i;
+			col = j;
+			sim = '#';
+			while (row > 0)
+			{
+				if (row % 3 == 1 && col % 3 == 1)
+				{
+					sim = ' ';
+				}
+				row /= 3;
+				col /= 3;
+			}
+			printf("%c", sim);
+		}
+		printf("\n");
+	}
+}
---
+++ 0x0B-menger/menger.h
@@ -0,0 +1,6 @@
+#ifndef __MENGER__
+#define __MENGER__
+
+void menger(int level);
+
+#endif
