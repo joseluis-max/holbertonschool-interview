7a4857a972054c4eaa8dda6898f93a864427bef6
Joseph Valdiviezo <luisj.valdiviezo20@gmail.com>
2022-07-21 20:31:56 -0500

    Add: N queens

--- 0x0C-nqueens/0-nqueens.py
+++ 0x0C-nqueens/0-nqueens.py
@@ -1,90 +1,68 @@
 #!/usr/bin/python3
+"""N Queens"""
+import sys
 
 
-from sys import argv
+def format_requirements(board):
+    """Prints according to requirements"""
+    ret = []
+    for i in range(len(board)):
+        colIdx = board[i].index(1)
+        ret.append([i, colIdx])
+    print(ret)
 
 
-if len(argv) != 2:
-    print("Usage: nqueens N")
-    exit(1)
-
-n_queens = argv[1]
-
-try:
-    n_queens = int(n_queens)
-except ValueError:
-    print("N must be a number")
-    exit(1)
-
-if n_queens < 4:
-    print("N must be at least 4")
-    exit(1)
-
-non_attacking_queens = []
-rows_attacked = []
-cols_attacked = []
-
-
-def is_safe(new_queen):
-    if new_queen[0] in cols_attacked:
-        return False
-    if new_queen[1] in rows_attacked:
-        return False
-
-    """ Checks diagonal up left """
-    i = new_queen[0]
-    j = new_queen[1]
-    while i >= 0 and j >= 0:
-        if [i, j] in non_attacking_queens:
+def is_valid_queen(board, curCol, row, n):
+    """Checks if element is a valid queen"""
+    # Check prev columns
+    for i in range(curCol):
+        if board[row][i] == 1:
             return False
-
+    # Check for upper diagonal
+    i = row
+    j = curCol
+    while i >= 0 and j >= 0:
+        if board[i][j] == 1:
+            return False
         i -= 1
         j -= 1
-
-    """ Checks diagonal up right """
-    i = new_queen[0]
-    j = new_queen[1]
-    while j < n_queens and i >= 0:
-        if [i, j] in non_attacking_queens:
+    # Check for lower diagonal
+    i = row
+    j = curCol
+    while i < n and j >= 0:
+        if board[i][j] == 1:
             return False
-
-        i -= 1
-        j += 1
-
+        i += 1
+        j -= 1
     return True
 
 
-def search_queens(col_start):
-    for i in range(n_queens):
-        if i != 0:
-            col_start = 0
-        for j in range(col_start, n_queens):
-            if [i, j] not in non_attacking_queens:
-                if is_safe([i, j]):
-                    non_attacking_queens.append([i, j])
-                    cols_attacked.append(i)
-                    rows_attacked.append(j)
-
-    return non_attacking_queens
-
-
-def finds_all_possibilities():
-    col_start = 0
-
-    while True:
-        non_attacking_queens.clear()
-        rows_attacked.clear()
-        cols_attacked.clear()
-        possible_solution = search_queens(col_start)
-        """ print(possible_solution) """
-
-        if len(possible_solution) == n_queens:
-            print(possible_solution)
-
-        col_start += 1
-
-        if col_start == n_queens:
-            break
+def nQueens(board, curCol, n):
+    """Recursive call that places queens in all the
+    posible positions of the board"""
+    stat = False
+    if curCol == n:
+        format_requirements(board)
+        return True
+    for row in range(0, n):
+        if is_valid_queen(board, curCol, row, n):
+            board[row][curCol] = 1
+            stat = nQueens(board, curCol + 1, n) or stat
+            board[row][curCol] = 0
+    return stat
 
 
-finds_all_possibilities()
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        print("Usage: nqueens N")
+        exit(1)
+    if not sys.argv[1].isdigit():
+        print("N must be a number")
+        exit(1)
+    n = int(sys.argv[1])
+    if n < 4:
+        print("N must be at least 4")
+        exit(1)
+    board = [[0 for i in range(n)] for j in range(n)]
+    # first col is 0
+    nQueens(board, 0, n)
